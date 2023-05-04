7e63cc6fede8a5bd932ac8f4f99f444e2c6e47ca
Zachary Keyes <56801478+imzoc@users.noreply.github.com>
2023-05-04 14:09:18 -0700

    more of last commit

--- src/process_all.py
+++ src/process_all.py
@@ -10,7 +10,7 @@ import handler
 
 def main():
     request_handler = handler.RequestHandler()
-    results_list = request_handler.take_n_images(3)
+    results_list = request_handler.take_all_images()
     request_handler.write_results_to_file(results_list)
 
 
